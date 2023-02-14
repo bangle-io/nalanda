00b9d1b44b0a7c922df745cadec134362be9e3dd
Kushan Joshi <0o3ko0@gmail.com>
2023-02-14 00:10:39 -0500

    bring back react

---
+++ src/react/__tests__/use-slice.test.ts
@@ -0,0 +1,81 @@
+import { createKey, slice } from '../../vanilla/create';
+import { timeoutSchedular } from '../../vanilla/effect';
+import { expectType } from '../../vanilla/internal-types';
+import { Store } from '../../vanilla/store';
+import { createUseSliceHook } from '../use-slice';
+
+export type IsNeverAny<Type> = true extends false & Type ? never : Type;
+export const isNeverAny = <Type>(_: IsNeverAny<Type>) => {};
+
+const testSlice1 = slice({
+  key: createKey('test-1', [], { num: 4 }),
+  actions: {
+    increment: (opts: { increment: boolean }) => (state) => {
+      return { ...state, num: state.num + (opts.increment ? 1 : 0) };
+    },
+    decrement: (opts: { decrement: boolean }) => (state) => {
+      return { ...state, num: state.num - (opts.decrement ? 1 : 0) };
+    },
+  },
+});
+
+const testSlice2 = slice({
+  key: createKey(
+    'test-2',
+    [],
+    { name: 'tame' },
+    {
+      fancy: (state) => state.name.padEnd(10, ' ').toUpperCase(),
+    },
+  ),
+  actions: {
+    prefix: (prefix: string) => (state) => {
+      return { ...state, name: prefix + state.name };
+    },
+    padEnd: (length: number, pad: string) => (state) => {
+      return { ...state, name: state.name.padEnd(length, pad) };
+    },
+    uppercase: () => (state) => {
+      return { ...state, name: state.name.toUpperCase() };
+    },
+  },
+});
+
+const testSlice3 = slice({
+  key: createKey('test-3', [], { name: 'tame' }),
+  actions: {
+    lowercase: () => (state) => {
+      return { ...state, name: state.name.toLocaleLowerCase() };
+    },
+  },
+});
+
+test.skip('useSlice with store, types are correct', () => {
+  const store = Store.create({
+    storeName: 'test-store',
+    scheduler: timeoutSchedular(0),
+    state: [testSlice1, testSlice2],
+  });
+
+  const useSlice = createUseSliceHook(store);
+
+  // @ts-expect-error - should not be able to use a slice that isn't registered
+  useSlice(testSlice3);
+  let [val] = useSlice(testSlice1);
+
+  isNeverAny(val);
+  expectType<{ num: number }>(val);
+
+  let [val2] = useSlice(testSlice1, (state) => state.num + 1);
+
+  isNeverAny(val2);
+  expectType<number>(val2);
+
+  let [val3] = useSlice(testSlice2, (state) => {
+    expectType<string>(state.fancy);
+    return state.name + state.fancy;
+  });
+
+  isNeverAny(val3);
+  expectType<string>(val3);
+});
---
+++ src/react/use-slice.ts
@@ -0,0 +1,48 @@
+import { useDebugValue, useState } from 'react';
+import useSyncExternalStoreExports from 'use-sync-external-store/shim';
+import { AnySlice, BareStore } from '../vanilla/public-types';
+
+import { StoreState } from '../vanilla/state';
+import type { Store } from '../vanilla/store';
+
+const { useSyncExternalStore } = useSyncExternalStoreExports;
+
+export function createUseSliceHook<SSL extends AnySlice>(
+  store: BareStore<SSL>,
+) {
+  function useSlice<SL extends AnySlice>(
+    sl: SL['key'] extends SSL['key'] ? SL : never,
+  ): [SL['config']['initState'], BareStore<SSL>['dispatch']];
+  function useSlice<SL extends AnySlice, SLS>(
+    sl: SL['key'] extends SSL['key'] ? SL : never,
+    selector: (state: ReturnType<SL['resolveState']>) => SLS,
+  ): [SLS, BareStore<SSL>['dispatch']];
+  function useSlice<SL extends AnySlice, SLS>(
+    sl: SL['key'] extends SSL['key'] ? SL : never,
+    selector?: (p: SL['config']['initState']) => SLS,
+  ): [SLS, BareStore<SSL>['dispatch']] {
+    const [subscribe] = useState(() => {
+      return (cb: () => void) => {
+        return (store as Store)._tempRegisterOnSyncChange(sl, cb);
+      };
+    });
+
+    const [snap] = useState(() => {
+      return () => {
+        const result = sl.resolveState(store.state as StoreState<any>);
+        if (selector) {
+          return selector(result);
+        }
+        return result;
+      };
+    });
+
+    const data = useSyncExternalStore(subscribe, snap);
+
+    useDebugValue(data);
+
+    return [data, store.dispatch];
+  }
+
+  return useSlice;
+}
