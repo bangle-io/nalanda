1d9f857b0448592b21e8f6a2ca224426756d83c2
Kushan Joshi <0o3ko0@gmail.com>
2023-09-30 19:24:36 -0400

    documentation

--- documentation/components/CodeBlock.tsx
+++ documentation/components/CodeBlock.tsx
@@ -9,7 +9,55 @@ import {
 import { sandpackDark } from '@codesandbox/sandpack-themes';
 import { useTheme } from 'next-themes';
 import { useEffect, useState } from 'react';
+// @ts-expect-error - no types
+import rawNsmCode from '../dist/nsm-docs-bundle/index.mjs?raw';
+import prettier from 'prettier';
 
-export function CodeBlock() {
-  return <Sandpack />;
+export function CodeBlockVanilla({ children }: { children: string }) {
+  const code = `
+${children.trim()}
+  `.trim();
+
+  return (
+    <Sandpack
+      options={{
+        layout: 'console',
+      }}
+      files={{
+        '/index.ts': {
+          code: `${code}`.trim(),
+        },
+        '/node_modules/nalanda/package.json': {
+          hidden: true,
+          code: JSON.stringify({
+            name: 'nalanda',
+            main: './index.mjs',
+          }),
+        },
+        '/node_modules/nalanda/index.mjs': {
+          hidden: true,
+          code: rawNsmCode,
+        },
+      }}
+      theme="light"
+      template="vanilla-ts"
+    />
+  );
+}
+export function CodeBlock({ children }: { children: string }) {
+  return (
+    <Sandpack
+      options={{
+        layout: 'console',
+      }}
+      files={{
+        '/App.tsx': {
+          // not doing trim causes weird errors
+          code: children.trim(),
+        },
+      }}
+      theme="light"
+      template={'react-ts'}
+    />
+  );
 }
