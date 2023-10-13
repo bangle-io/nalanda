dfa1bf79002950a35e18db5420a512902c0838e1
Kushan Joshi <0o3ko0@gmail.com>
2023-10-12 23:35:49 -0400

    fixe

--- documentation/theme.config.tsx
+++ documentation/theme.config.tsx
@@ -1,11 +1,16 @@
-import type { DocsThemeConfig } from 'nextra-theme-docs';
+import type { DocsThemeConfig } from "nextra-theme-docs";
+import Image from "next/image";
 
 const config: DocsThemeConfig = {
-  logo: <span>Nalanda State</span>,
+  logo: (
+    <span className="animate-gradientAnimation font-black inline-block text-transparent bg-clip-text bg-gradient-to-r dark:from-red-100 dark:to-amber-500 from-red-600 to-amber-500">
+      Nalanda
+    </span>
+  ),
   docsRepositoryBase:
-    'https://github.com/bangle-io/nalanda/blob/dev/documentation/pages',
+    "https://github.com/bangle-io/nalanda/blob/dev/documentation/pages",
   project: {
-    link: 'https://github.com/bangle-io/nalanda',
+    link: "https://github.com/bangle-io/nalanda",
   },
   footer: {
     text: (
