8824d8d873c24f6660e1ed27e77069374356080c
Kushan Joshi <0o3ko0@gmail.com>
2023-10-18 22:57:03 -0400

    docs: tutorial

--- documentation/components/CodeBlock.tsx
+++ documentation/components/CodeBlock.tsx
@@ -1,7 +1,12 @@
-import { Sandpack } from '@codesandbox/sandpack-react';
-import { sandpackDark } from '@codesandbox/sandpack-themes';
 import { useTheme } from 'next-themes';
-import { useEffect, useState } from 'react';
+import {
+  SandpackProvider,
+  SandpackLayout,
+  SandpackPreview,
+  SandpackCodeEditor,
+  Sandpack,
+} from '@codesandbox/sandpack-react';
+import { useEffect, useMemo, useState } from 'react';
 import { Callout, Tabs, Tab } from 'nextra-theme-docs';
 // @ts-expect-error - not worth the effort to fix
 import rawNsmCode from '../../packages/core/dist/index.mjs?raw';
@@ -10,9 +15,100 @@ import rawNsmReactCode from '../../packages/react/dist/index.mjs?raw';
 
 // @ts-expect-error - not worth the effort to fix
 import rawUseSyncExternalStoreCode from 'use-sync-external-store/cjs/use-sync-external-store-shim.production.min.js?raw';
-import prettier from 'prettier';
 
-import Image from 'next/image';
+const nalandaFiles = {
+  '/node_modules/use-sync-external-store/shim/package.json': {
+    hidden: true,
+    code: JSON.stringify({
+      name: 'use-sync-external-store/shim',
+      main: './index.js',
+    }),
+  },
+  '/node_modules/use-sync-external-store/shim/index.js': {
+    hidden: true,
+    code: rawUseSyncExternalStoreCode,
+  },
+  '/node_modules/@nalanda/core/package.json': {
+    hidden: true,
+    code: JSON.stringify({
+      name: '@nalanda/core',
+      main: './index.mjs',
+    }),
+  },
+  '/node_modules/@nalanda/core/index.mjs': {
+    hidden: true,
+    code: rawNsmCode,
+  },
+  '/node_modules/@nalanda/react/package.json': {
+    hidden: true,
+    code: JSON.stringify({
+      name: '@nalanda/react',
+      main: './index.mjs',
+    }),
+  },
+  '/node_modules/@nalanda/react/index.mjs': {
+    hidden: true,
+    code: rawNsmReactCode,
+  },
+};
+export function CodeBlockTabs({
+  height,
+  closableTabs = true,
+  otherSources = {},
+  appSource,
+}: {
+  height?: number | undefined;
+  closableTabs: boolean;
+  appSource: string;
+  otherSources: Record<string, string>;
+}) {
+  const { theme } = useTheme();
+  const code = `
+${appSource.trim()}
+  `.trim();
+
+  const otherSourcesFiles = useMemo(() => {
+    return Object.fromEntries(
+      Object.entries(otherSources).map(([key, value]) => {
+        return [key, value.trim()];
+      }),
+    );
+  }, [otherSources]);
+  return (
+    <SandpackProvider
+      template="react-ts"
+      options={{
+        externalResources: [
+          'https://cdn.jsdelivr.net/npm/water.css@2/out/water.css',
+        ],
+      }}
+      theme={theme === 'light' ? 'light' : 'dark'}
+      files={{
+        ...otherSourcesFiles,
+        '/App.tsx': code,
+        ...nalandaFiles,
+      }}
+    >
+      <SandpackLayout>
+        <SandpackCodeEditor
+          showTabs
+          showLineNumbers={false}
+          showInlineErrors
+          wrapContent
+          closableTabs={closableTabs}
+          style={{
+            height: height,
+          }}
+        />
+        <SandpackPreview
+          style={{
+            height: height,
+          }}
+        />
+      </SandpackLayout>
+    </SandpackProvider>
+  );
+}
 
 export function CodeBlockVanilla({
   height,
@@ -30,64 +126,17 @@ ${children.trim()}
     <Sandpack
       template="react-ts"
       options={{
+        externalResources: [
+          'https://cdn.jsdelivr.net/npm/water.css@2/out/water.css',
+        ],
         editorHeight: height,
         editorWidthPercentage: 66, // default - 50
       }}
       theme={theme === 'light' ? 'light' : 'dark'}
       files={{
         '/App.tsx': code,
-        '/node_modules/use-sync-external-store/shim/package.json': {
-          hidden: true,
-          code: JSON.stringify({
-            name: 'use-sync-external-store/shim',
-            main: './index.js',
-          }),
-        },
-        '/node_modules/use-sync-external-store/shim/index.js': {
-          hidden: true,
-          code: rawUseSyncExternalStoreCode,
-        },
-        '/node_modules/@nalanda/core/package.json': {
-          hidden: true,
-          code: JSON.stringify({
-            name: '@nalanda/core',
-            main: './index.mjs',
-          }),
-        },
-        '/node_modules/@nalanda/core/index.mjs': {
-          hidden: true,
-          code: rawNsmCode,
-        },
-        '/node_modules/@nalanda/react/package.json': {
-          hidden: true,
-          code: JSON.stringify({
-            name: '@nalanda/react',
-            main: './index.mjs',
-          }),
-        },
-        '/node_modules/@nalanda/react/index.mjs': {
-          hidden: true,
-          code: rawNsmReactCode,
-        },
+        ...nalandaFiles,
       }}
-    />
-  );
-}
-
-export function CodeBlock({ children }: { children: string }) {
-  const { theme } = useTheme();
-
-  return (
-    <Sandpack
-      options={{}}
-      files={{
-        '/App.tsx': {
-          // not doing trim causes weird errors
-          code: children.trim(),
-        },
-      }}
-      theme={theme === 'light' ? 'light' : 'dark'}
-      template={'react-ts'}
     />
   );
 }
